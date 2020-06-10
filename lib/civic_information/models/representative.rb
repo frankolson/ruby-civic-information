4fe796a4cee8d1af0898fb8373a8e1d7351b7230
Will Olson <will.olson11@gmail.com>
2020-06-09 21:29:27 -0700

    Allow users to provide their own representative roles

--- lib/civic_information/models/representative.rb
+++ lib/civic_information/models/representative.rb
@@ -2,12 +2,12 @@ module CivicInformation
   class Representative
     attr_accessor :name, :phones
 
-    def self.where(address: nil)
+    def self.where(address: nil, roles: nil)
       response = CivicInformation.get "/representatives", query: {
           key: CivicInformation.configuration.google_api_key,
           address: address,
-          roles: ['legislatorLowerBody', 'legislatorUpperBody']
-        }
+          roles: roles
+        }.delete_if { |k, v| v.nil? }
       officials = response.parsed_response["officials"] || []
 
       officials.map do |representative|
