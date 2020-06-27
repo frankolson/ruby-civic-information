df8e711edc741a30589bfa86b999519b5764f050
Will Olson <will.olson11@gmail.com>
2020-06-27 09:04:02 -0700

    Add addresses to officials

--- lib/civic_information/models/representative/official.rb
+++ lib/civic_information/models/representative/official.rb
@@ -1,10 +1,17 @@
 module CivicInformation
   class Representative::Official
-    attr_accessor :name, :phones
+    attr_accessor :name, :phones, :addresses
 
     def initialize(official_json)
       @name = official_json["name"]
-      @phones = official_json["phones"]
+      @phones = official_json["phones"] || []
+      @addresses = build_addresses(official_json["address"] || [])
     end
+
+    private
+
+      def build_addresses(address_json)
+        address_json.map { |address| Representative::Address.new(address) }
+      end
   end
 end
